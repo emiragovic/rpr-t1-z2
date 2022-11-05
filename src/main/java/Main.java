8332fb47ed83409771db8eca2121bf969d224e5c
emiragovic <eagovic1@etf.unsa.ba>
2022-11-05 16:58:06 +0100

    formatting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,15 @@
-import org.w3c.dom.ls.LSInput;
-
 import java.util.Scanner;
 
 public class Main {
     public static int sumaCifara(int n) {
         int suma = 0;
-        while (n != 0){
-            suma += n%10;
+        while (n != 0) {
+            suma += n % 10;
             n /= 10;
+
+
         }
-            return suma;
+        return suma;
     }
 
     public static void main(String[] args) {
@@ -18,9 +18,8 @@ public class Main {
         Scanner unos = new Scanner(System.in);
         n = unos.nextInt();
         System.out.println("Brojevi od 1 do n koji su djeljivi sa sumom svojih cifara:");
-        for(int i = 1; i<n; i++){
-            if(i%sumaCifara(i) == 0)
-                System.out.println(i);
+        for (int i = 1; i < n; i++) {
+            if (i % sumaCifara(i) == 0) System.out.println(i);
         }
     }
 }
